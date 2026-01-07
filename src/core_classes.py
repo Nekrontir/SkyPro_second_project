a090a0bfc455bd41b9b187b0af6d266136c61446
Viktor Schastlivenko <klarkkent0314@gmail.com>
2026-01-07 21:16:18 +0300

    add moduls

--- src/core_classes.py
+++ src/core_classes.py
@@ -1,7 +1,10 @@
 from typing import Any, Dict, List, Union
 
+from src.base_product_class import BaseProduct
+from src.mixin_class import MixinInfoProduct
 
-class Product:
+
+class Product(BaseProduct, MixinInfoProduct):
     """
     Класс для представления товара в магазине.
     """
@@ -12,10 +15,12 @@ class Product:
     all_products: list = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
         Product.all_products.append(self)
 
     @classmethod
@@ -96,3 +101,8 @@ class Category:
         for product in self.__products:
             total_number += product.quantity
         return f"{self.name}, количество продуктов: {total_number} шт."
+
+
+if __name__ == "__main__":
+    print(Product.__mro__)
+    t_obj = Product("t", "tt", 3.0, 100)
